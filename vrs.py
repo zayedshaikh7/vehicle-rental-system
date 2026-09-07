4b66b87e79a4f99d8bd397a977075633481f7764
Zayed Shaikh <143567693+zayedshaikh7@users.noreply.github.com>
2026-09-07 11:14:57 +0530

    Upgrade Vehicle Rental System to version 2.0
    Updated version to 2.0 and added vehicle search feature.

--- vrs.py
+++ vrs.py
@@ -1,5 +1,7 @@
-# Vehicle Rental System - Version 1.1
-# Features: rent and return vehicles, rental cost calculation
+# Vehicle Rental System - Version 2.0
+# Features: rent/return vehicles, rental cost calculation, online vehicle search
+
+vehicles = ["Car", "Bike", "SUV"]
 
 def rent_vehicle(vehicle_id, customer_id):
     print("Vehicle", vehicle_id, "rented to customer", customer_id)
@@ -11,3 +13,9 @@ def calculate_rental_cost(days, rate=500):
     cost = days * rate
     print("Rental Cost = Rs.", cost)
     return cost
+
+def search_vehicle(vehicle_type):
+    if vehicle_type in vehicles:
+        print(vehicle_type, "is available")
+    else:
+        print(vehicle_type, "not found")
