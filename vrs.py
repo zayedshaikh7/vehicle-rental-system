59e1bf29ef66ac91964a4a44fa36cbd576493ae4
Zayed Shaikh <143567693+zayedshaikh7@users.noreply.github.com>
2026-09-07 11:08:08 +0530

    Delete vrs.py

--- vrs.py
+++
@@ -1,8 +0,0 @@
-# Vehicle Rental System - Version 1.0
-# Feature: rent and return vehicles
-
-def rent_vehicle(vehicle_id, customer_id):
-    print("Vehicle", vehicle_id, "rented to customer", customer_id)
-
-def return_vehicle(vehicle_id):
-    print("Vehicle", vehicle_id, "returned")
